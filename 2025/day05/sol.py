ee66cdc5f9f79ce712d83470556c1561e590bf0d
esmecat <71032999+esmaycat@users.noreply.github.com>
2025-12-05 19:01:52 +0000

    day 5

---
+++ 2025/day05/sol.py
@@ -0,0 +1,19 @@
+# AOC 2025 Day 5 – expect input on stdin
+
+import operator as op
+
+b, ingredients = open(0).read().split('\n\n')
+ranges = sorted([eval(r.replace('-', ',')) for r in b.split('\n')], key=op.itemgetter(0))
+ingredients = [*map(int, ingredients.split('\n'))]
+
+print(sum(any(a <= i <= b for a, b in ranges) for i in ingredients)) # Part 1
+
+p2 = 0
+a, b = ranges[0]
+for p, q in ranges[1:]:
+    if p > b:
+        p2 += b - a + 1
+        a, b = p, q
+    else: b = max(b, q)
+
+print(p2 + b - a + 1)                                                # Part 2
