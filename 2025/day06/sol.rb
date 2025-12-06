80d3c0f93e2dabd32ff70572bc39a447a9acc5ff
esmecat <71032999+esmaycat@users.noreply.github.com>
2025-12-06 20:38:31 +0000

    use String.pack in day 6

--- 2025/day06/sol.rb
+++ 2025/day06/sol.rb
@@ -4,14 +4,13 @@ ns = $<.readlines
 last = ns.pop
 $ops = last.split.map(&:to_sym)
 
-def do_sums(a)
-    a.zip($ops).map { _1.inject _2 }.sum
-end
+def do_sums(ns) ns.zip($ops).map { _1.inject _2 }.sum end
 
-puts(do_sums ns.map { _1.split.map(&:to_i )}.transpose)          # Part 1
+p do_sums ns.map { _1.split.map(&:to_i ) }.transpose           # Part 1
 
-col_widths = last.split(/\*|\+/)[1..].map(&:length)
-col_widths[-1] += 1
-puts(do_sums ns.map { |l|
-    i = 0; col_widths.map { |w| i += w + 1; l.slice(i-w-1, w).chars }
-}.transpose.map { _1[0].zip(*_1[1..]).map(&:join).map(&:to_i) }) # Part 2
+ws = last.split(/\*|\+/)[1..].map(&:length)
+ws[-1] += 1
+t = ws.map { |w| "a#{w}" }.join("x")
+p do_sums ns.map { _1.unpack(t).map(&:chars) }.transpose.map { # Part 2
+    _1.transpose.map(&:join).map(&:to_i) 
+}
